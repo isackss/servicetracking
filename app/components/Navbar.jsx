1f2e47dba731cd6c13ba1581bc577a8df3e7aabd
Isacks <eddisacks@gmail.com>
2024-09-18 17:22:44 -0500

    working on navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between px-2">
+        <div>LOGO</div>
+        <ul>
+          <li>Registro</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
